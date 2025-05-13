5d6b0aec1cf0edbf583a46dc39b694e3bb84d937
Joseph-Marlow <jmarlow@byupathway.edu>
2025-05-12 17:57:05 -0700

    completed the Abstraction activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Jobs job1 = new Jobs();
+        job1._jobTitle = "Web Developer";
+        job1._company = "Intel";
+        job1._startYear = 1998;
+        job1._endYear = 2024;
+
+        Jobs job2 = new Jobs();
+        job2._jobTitle = "Dev Team Manager";
+        job2._company = "Microsoft";
+        job2._startYear = 2025;
+        job2._endYear = 2027;
+
+        Resume myResume = new Resume();
+        myResume._name = "Guisseppe Martino";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
